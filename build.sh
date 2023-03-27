7544be569bf18537614b439bb284f9c0e5b0c6d3
AnaxMorden <79524821+AnaxMorden@users.noreply.github.com>
2023-03-27 11:43:15 +0000

    Test update

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 set -xe
 
 [ -d build ] || git clone https://gitlab.com/ubports/community-ports/halium-generic-adaptation-build-tools -b halium-11 build
-./build/build.sh "$@"
+./build/build-kernel.sh "$@"
